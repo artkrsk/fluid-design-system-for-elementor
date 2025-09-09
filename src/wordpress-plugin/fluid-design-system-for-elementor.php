891407c23f76ab67b50f61f8ff6f605ecaba1399
Artem Semkin <art.krsk@gmail.com>
2025-09-09 21:27:37 +0800

    chore: bump version to 1.1.1

--- src/wordpress-plugin/fluid-design-system-for-elementor.php
+++ src/wordpress-plugin/fluid-design-system-for-elementor.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Fluid Design System for Elementor
  * Description: Provides comprehensive fluid spacing and typography system for Elementor for smoother and consistent responsive design
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: Artem Semkin
  * Author URI: https://artemsemkin.com
  * License: GPLv3
